cc25e5802f7239d07065002c3a66ca655200f1b5
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-23 16:27:28 -0700

    add walletconnect

--- src/helpers/web3Modal.ts
+++ src/helpers/web3Modal.ts
@@ -1,8 +1,18 @@
+import WalletConnectProvider from '@walletconnect/web3-provider'
 import Web3Modal from 'web3modal'
 
 export default new Web3Modal({
   cacheProvider: true,
   theme: 'dark',
   disableInjectedProvider: false,
-  providerOptions: {},
+  providerOptions: {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        rpc: {
+          1: 'https://cloudflare-eth.com/v1/mainnet',
+        },
+      },
+    },
+  },
 })
