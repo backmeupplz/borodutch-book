409bf9b607e51e67782c9d6b325aa848e236f3ef
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-09-23 20:35:59 -0700

    fix %23

--- src/hooks/useSlug.ts
+++ src/hooks/useSlug.ts
@@ -2,5 +2,5 @@ import { useLocation } from 'wouter'
 
 export default function () {
   const [location] = useLocation()
-  return location.substring(1).split('?')[0]
+  return location.substring(1).split('?')[0]?.replace('%23', '#')
 }
